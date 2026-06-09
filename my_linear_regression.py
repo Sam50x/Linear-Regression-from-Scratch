c60598a44478fb1c6da15499c9bb99962775d3e0
SAM50x <149119430+Sam50x@users.noreply.github.com>
2026-06-09 12:54:57 +0200

    basic training and prediction

---
+++ my_linear_regression.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+class LinearRegression:
+    def __init__(self):
+        print('Hello, Regression')
+
+    def fit(self, X, y, epochs=1000, learning_rate=0.01):
+        p = X.shape[1]
+        np.random.seed(42)
+
+        b = np.random.random(1)
+        w = np.random.random(p)
+
+        losses = []
+
+        for epoch in range(epochs):
+            y_pred = self.__predict(X, w, b)
+
+            loss = self.__calculate_loss(y, y_pred)
+            losses.append(loss)
+
+            grad_b, grad_w = self.__calculate_gradients(X, y, y_pred)
+
+            b = b - learning_rate * grad_b
+            w = w - learning_rate * grad_w
+
+            if epoch % 100 == 0:
+                print(f'MSE loss at epoch {epoch} = {loss}')
+
+        print(f'MSE loss at epoch {epochs} = {losses[-1]}')
+
+        self.losses = losses
+        self.w = w
+        self.b = b
+
+    def predict(self, X):
+        y_pred = np.dot(X, self.w.T) + self.b
+        return y_pred
+
+    def __predict(self, X, w, b):
+        y_pred = np.dot(X, w.T) + b
+        return y_pred
+
+    def __calculate_loss(self, y_true, y_pred):
+        n = len(y_true)
+
+        loss = np.sum((y_pred - y_true) ** 2) / n
+
+        return loss
+
+    def __calculate_gradients(self, X, y_true, y_pred):
+        n = len(y_true)
+
+        grad_b = 2 * (np.sum(y_pred - y_true)) / n
+        grad_w = 2 * (np.dot((y_pred - y_true).T, X)) / n
+
+        return grad_b, grad_w
